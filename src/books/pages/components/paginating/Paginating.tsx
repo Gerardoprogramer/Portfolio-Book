9bcb19c2e292c9ee511357f49e923cd987c871af
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2025-09-30 09:14:50 -0600

    refactor: Improve formatting and consistency in Paginating component

--- src/books/pages/components/paginating/Paginating.tsx
+++ src/books/pages/components/paginating/Paginating.tsx
@@ -1,36 +1,36 @@
 import { Button } from "@/components/ui/button"
 
-interface Props{
-    currentPage: number,
-    totalPages: number,
-    setCurrentPage: React.Dispatch<React.SetStateAction<number>>
+interface Props {
+  currentPage: number,
+  totalPages: number,
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
 
 }
 
-export const Paginating = ({currentPage, setCurrentPage, totalPages}: Props) => {
+export const Paginating = ({ currentPage, setCurrentPage, totalPages }: Props) => {
   return (
-        <div className="flex justify-center items-center gap-4 mt-12">
-          <Button
-            disabled={currentPage === 1}
-            onClick={() => setCurrentPage((p) => p - 1)}
-            className="bg-emerald-500 hover:bg-emerald-400 text-zinc-950 font-semibold px-8 py-4 text-lg rounded-none relative overflow-hidden group"
-            style={{ clipPath: "polygon(0 0, 100% 0, 90% 100%, 10% 100%)" }}
-          >
-            Anterior
-          </Button>
+    <div className="flex justify-center items-center gap-4 mt-12">
+      <Button
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage((p) => p - 1)}
+        className="bg-emerald-500 hover:bg-emerald-400 text-zinc-950 font-semibold px-4 sm:px-8 py-2 sm:py-4 text-sm sm:text-lg rounded-none relative overflow-hidden group"
+        style={{ clipPath: "polygon(0 0, 100% 0, 90% 100%, 10% 100%)" }}
+      >
+        Anterior
+      </Button>
 
-          <span className="text-zinc-400">
-            Página {currentPage} de {totalPages}
-          </span>
+      <span className="text-zinc-400 text-sm sm:text-base mt-2 sm:mt-0">
+        Página {currentPage} de {totalPages}
+      </span>
 
-          <Button
-            disabled={currentPage === totalPages}
-            onClick={() => setCurrentPage((p) => p + 1)}
-            className="bg-emerald-500 hover:bg-emerald-400 text-zinc-950 font-semibold px-8 py-4 text-lg rounded-none relative overflow-hidden group"
-            style={{ clipPath: "polygon(0 0, 100% 0, 90% 100%, 10% 100%)" }}
-          >
-            Siguiente
-          </Button>
-        </div>
+      <Button
+        disabled={currentPage === totalPages}
+        onClick={() => setCurrentPage((p) => p + 1)}
+        className="bg-emerald-500 hover:bg-emerald-400 text-zinc-950 font-semibold px-4 sm:px-8 py-2 sm:py-4 text-sm sm:text-lg rounded-none relative overflow-hidden group"
+        style={{ clipPath: "polygon(0 0, 100% 0, 90% 100%, 10% 100%)" }}
+      >
+        Siguiente
+      </Button>
+    </div>
   )
 }
